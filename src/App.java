caff98281e47a1562016cbc63c3d4acdbc9798a3
isobutilo09 <alfuenal@gmail.com>
2026-05-07 19:51:22 +0200

    He añadido ej8 y jose nose que

--- src/App.java
+++ src/App.java
@@ -1,11 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Jose");
-
-        System.out.println("Escrito por alvaro");
+        System.out.println("Jose ejercicio 8B");
+        System.out.println("Escrito por alvaro sdf");
         System.out.println("ejercicio 7");
-        
         System.out.println("ejercicio 8b");
-        
     }
 }
